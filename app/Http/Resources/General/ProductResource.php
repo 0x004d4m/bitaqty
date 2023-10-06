d86219370022042e970b868399161a30836f739f
Adam Sabbah <Adam Sabbah>
2023-10-06 11:42:04 +0300

    updates

--- app/Http/Resources/General/ProductResource.php
+++ app/Http/Resources/General/ProductResource.php
@@ -22,7 +22,7 @@ class ProductResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id ,
+            'id' => $this->id,
             'name' => $this->name,
             'description' => $this->description,
             'unavailable_notes' => $this->unavailable_notes,
