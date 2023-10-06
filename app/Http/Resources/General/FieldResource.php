c73f3541d1d619193a88dffc57276885f3a3e681
Adam Sabbah <Adam Sabbah>
2023-10-06 11:50:05 +0300

    updates

--- app/Http/Resources/General/FieldResource.php
+++ app/Http/Resources/General/FieldResource.php
@@ -8,16 +8,6 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class FieldResource extends JsonResource
 {
-    protected $order_id;
-    protected $product_id;
-
-    public function __construct($resource, $order_id=null, $product_id=null)
-    {
-        parent::__construct($resource);
-        $this->order_id = $order_id;
-        $this->product_id = $product_id;
-    }
-
     /**
      * Transform the resource into an array.
      *
@@ -25,7 +15,7 @@ class FieldResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $answer = FieldsAnswer::where('field_id', $this->id)->where('order_id', $this->order_id)->where('product_id', $this->product_id)->first();
+        $answer = FieldsAnswer::where('field_id', $this->id)->where('order_id', $this->additional['order_id'])->where('product_id', $this->additional['product_id'])->first();
         return [
             "id" => $this->id,
             "name" => $this->name,
--- app/Http/Resources/General/ProductResource.php
+++ app/Http/Resources/General/ProductResource.php
@@ -35,7 +35,7 @@ class ProductResource extends JsonResource
             'type' => new TypeResource($this->type),
             'category' => new CategoryResource($this->category),
             'subcategory' => new SubcategoryResource($this->subcategory),
-            'fields' => FieldResource::collection($this->subcategory->fields)->additional($this->order_id, $this->id),
+            'fields' => FieldResource::collection($this->subcategory->fields)->additional(["order_id"=>$this->order_id, "product_id"=>$this->id]),
         ];
     }
 }
