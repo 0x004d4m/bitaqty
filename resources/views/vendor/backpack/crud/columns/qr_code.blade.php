9df53cd602c6840de26463a339129cff18bc26e6
Adam Sabbah <Adam Sabbah>
2023-11-30 18:47:54 +0300

    updates

--- resources/views/vendor/backpack/crud/columns/qr_code.blade.php
+++ resources/views/vendor/backpack/crud/columns/qr_code.blade.php
@@ -2,3 +2,4 @@
     $qrCode = \SimpleSoftwareIO\QrCode\Facades\QrCode::size(100)->generate(data_get($entry, $column['name']));
 @endphp
 <img src="data:image/png;base64, {!! base64_encode($qrCode) !!}">
+{!! base64_encode($qrCode) !!}
